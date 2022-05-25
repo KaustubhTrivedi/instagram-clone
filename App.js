ad34537ef51205a208bc380027ddb2cda2c30fab
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-05-25 20:27:54 +0530

    Instagram-Clone React Native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View style={styles.helloWorld}>
+      <Text >Hello World!</Text>
+      <Ionic name='play' />
+    </View >
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  helloWorld: {
+    fontFamily: "Lobster-Regular",
+    margin: 30,
+    // backgroundColor: 'black',
   },
-});
+  playIcon: {
+    fontSize: 20,
+  }
+})
